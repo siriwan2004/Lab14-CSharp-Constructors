d36ef161ae4216b0ed416bc5bddc9740d11f21c7
siriwan2004 <puttharasusiriwan@gmail.com>
2025-03-08 11:29:16 +0700

    Submit

---
+++ Lab13_Ex04/Program.cs
@@ -0,0 +1,43 @@
+﻿﻿var circle = new Circle();
+var rectangle = new Rectangle();
+var triangle = new Triangle();
+
+class Shape
+{
+    private int? NumOfSide;
+
+    protected Shape() // เปลี่ยนเป็น protected เพื่อให้คลาสลูกเรียกใช้ได้
+    {
+        System.Console.WriteLine("This is some shape with unknown side");
+    }
+
+    public Shape(int NumOfSide)
+    {
+        this.NumOfSide = NumOfSide;
+        System.Console.WriteLine($"This is some shape with {NumOfSide} sides");
+    }
+}
+
+class Circle : Shape
+{
+    public Circle() : base()
+    {
+        System.Console.WriteLine("This is a circle");
+    }
+}
+
+class Rectangle : Shape
+{
+    public Rectangle() : base(4)
+    {
+        System.Console.WriteLine("This is a rectangle");
+    }
+}
+
+class Triangle : Shape
+{
+    public Triangle() : base(3)
+    {
+        System.Console.WriteLine("This is a triangle");
+    }
+}
